b64b670a9234c929fa0a55480125898c92eaf9bb
Alex4625 <tinolambut@gmail.com>
2026-01-10 22:02:11 +0800

    perbaikan katgoeri

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    use WithoutModelEvents;
+
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $categories = [
+            ['name' => 'Minuman', 'description' => 'Berbagai macam minuman'],
+            ['name' => 'Snack', 'description' => 'Makanan ringan dan camilan'],
+            ['name' => 'Makanan', 'description' => 'Makanan siap saji dan makanan umum'],
+            ['name' => 'Perlengkapan Tulis', 'description' => 'Alat tulis dan perlengkapan sekolah'],
+            ['name' => 'Perlengkapan Mandi', 'description' => 'Sabun, shampoo, dan perlengkapan mandi'],
+            ['name' => 'Bahan Baku Dapur', 'description' => 'Bahan-bahan untuk memasak'],
+        ];
+
+        foreach ($categories as $category) {
+            Category::create($category);
+        }
+    }
+}
