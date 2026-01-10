91255ce8f2fbd5de3b499bed23012f0687704942
Alex4625 <tinolambut@gmail.com>
2026-01-10 21:41:50 +0800

    tambahkan route

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,10 @@ use App\Http\Controllers\TransactionController;
 Route::resource('products', ProductController::class);
 Route::get('/report/chart', [ReportController::class, 'chart'])->name('report.chart');
 Route::resource('transactions', TransactionController::class);
+Route::get('/report/transactions/pdf', [ReportController::class, 'transactionsPdf'])->name('report.transactions.pdf');
+Route::resource('customers', CustomerController::class);
+Route::resource('categories', CategoryController::class);
+
 
 
 Route::get('/', function () {
