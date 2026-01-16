1b3a983431054dd27cbb4db7721bf72d76f84a11
wisatwan <wisatwan06@gmail.com>
2026-01-17 07:20:45 +0800

    tambah cloud baru

--- resources/views/transactions/show.blade.php
+++ resources/views/transactions/show.blade.php
@@ -252,8 +252,7 @@
                                     <div class="border rounded p-3 bg-white">
                                         <a href="{{ $transaction->payment_proof }}" target="_blank">
                                             <img src="{{ $transaction->payment_proof }}"
-                                                alt="Bukti Pembayaran"
-                                                style="max-width: 100%; max-height: 200px; object-fit: contain;">
+                                                alt="Bukti Pembayaran" style="max-width: 100%; max-height: 200px; object-fit: contain;">
                                         </a>
                                     </div>
                                 </div>
